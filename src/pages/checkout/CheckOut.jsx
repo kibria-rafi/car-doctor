bc2883c2a292cb93ff920eeeb20ddc3a634374fe
Kibria-Rafi <kibriarafi.bd.natore@gmail.com>
2024-05-01 01:14:09 +0600

    added checkout section

--- src/pages/checkout/CheckOut.jsx
+++ src/pages/checkout/CheckOut.jsx
@@ -1,14 +1,88 @@
 import { useLoaderData } from "react-router-dom";
 
-
 const CheckOut = () => {
-    const data = useLoaderData();
-    console.log(data)
-    return (
+  const data = useLoaderData();
+  const { _id, title, price } = data;
+  return (
+    <div>
         <div>
-            <h2>Book service</h2>
+            <h1 className="text-3xl text-red-400">{title}</h1>
         </div>
-    );
+      <form className="max-w-md mx-auto">
+        
+        <div className="md:flex gap-4">
+         
+          <div className="relative z-0 w-full mb-5 group">
+            <input
+              type="text"
+              name="firstName"
+              
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-[#FF3811] focus:outline-none focus:ring-0 focus:border-[#FF3811] peer"
+              placeholder=""
+              required
+            />
+            <label
+                htmlFor="floating_email"
+              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-[#FF3811] peer-focus:dark:text-[#FF3811] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              First Name
+            </label>
+          </div>
+          <div className="relative z-0 w-full mb-5 group">
+            <input
+              type="email"
+              name="floating_email"
+              id="floating_email"
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-[#FF3811] focus:outline-none focus:ring-0 focus:border-[#FF3811] peer"
+              placeholder=" "
+              required
+            />
+            <label
+                htmlFor="floating_email"
+              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-[#FF3811] peer-focus:dark:text-[#FF3811] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Email address
+            </label>
+          </div>
+        </div>
+        <div className="md:flex gap-4">
+         
+          <div className="relative z-0 w-full mb-5 group">
+            <input
+              type="email"
+              name="floating_email"
+              id="floating_email"
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-[#FF3811] focus:outline-none focus:ring-0 focus:border-[#FF3811] peer"
+              placeholder=" "
+              required
+            />
+            <label
+                htmlFor="floating_email"
+              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-[#FF3811] peer-focus:dark:text-[#FF3811] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Last Name
+            </label>
+          </div>
+          <div className="relative z-0 w-full mb-5 group">
+            <input
+              type="email"
+              name="floating_email"
+              id="floating_email"
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-[#FF3811] focus:outline-none focus:ring-0 focus:border-[#FF3811] peer"
+              placeholder=" "
+              required
+            />
+            <label
+                htmlFor="floating_email"
+              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-[#FF3811] peer-focus:dark:text-[#FF3811] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Email address
+            </label>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default CheckOut;
