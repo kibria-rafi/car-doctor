f21362677e3263884e01a93920c905468aa1f94a
Kibria-Rafi <kibriarafi.bd.natore@gmail.com>
2024-05-02 13:58:35 +0600

    add delete

--- src/pages/Bookings/Bookings.jsx
+++ src/pages/Bookings/Bookings.jsx
@@ -3,6 +3,7 @@ import { AuthContext } from "../../Providers/AuthProvider";
 import TableRow from "./TableRow";
 
 
+
 const Bookings = () => {
     const [bookings,setBookings]=useState([])
     const {user}= useContext(AuthContext)
@@ -13,12 +14,38 @@ const Bookings = () => {
         .then(res=>res.json())
         .then(data=>setBookings(data))
     },[user.email])
+
+    const handleDelete = id => {
+        const proceed = confirm('Are you sure you want to delete?');
+        if (proceed) {
+            fetch(`http://localhost:5000/bookings/${id}`, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    if (data.deletedCount > 0) {
+                        alert('Deleted successfully');
+                        const remaining = bookings.filter(booking => booking._id !== id);
+                        setBookings(remaining);
+                    } else {
+                        alert('Delete failed');
+                    }
+                })
+                .catch(error => {
+                    console.error('Error deleting booking:', error);
+                });
+        }
+    }
+
+
+
     return (
         <div>
             <h1 className="text-3xl"> {bookings.length} </h1>
             
       {
-       bookings.map(booking=> <TableRow key={booking._id} booking={booking}></TableRow>)
+       bookings.map(booking=> <TableRow handleDelete={handleDelete} key={booking._id} booking={booking}></TableRow>)
       }
       
     
--- src/pages/Bookings/TableRow.jsx
+++ src/pages/Bookings/TableRow.jsx
@@ -1,10 +1,12 @@
 
 
-const TableRow = ({booking}) => {
-    const {customerName,date,email,img,price,service} =booking;
+
+const TableRow = ({booking,handleDelete}) => {
+    const {_id,customerName,date,email,img,price,service} =booking;
+    
     return (
-        <div className="flex justify-between items-center">
-            <button  className="bg-black px-5 py-3 text-white rounded-full">X</button>
+        <div className="md:flex justify-between items-center">
+            <button onClick={()=>handleDelete(_id)}  className="bg-black px-5 py-3 text-white rounded-full">X</button>
             <img className="h-44 w- rounded-xl" src={img} alt="" />
             <h1 className="font-bold text-black text-xl">{customerName}</h1>
             <h1 className="font-bold text-black">{email}</h1>
